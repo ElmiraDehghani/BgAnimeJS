c7e2bd3259953fd5480746cd2c0235c31e93eed8
ElmiraDehghani <elmiradehghani.2000@gmail.com>
2023-11-06 13:23:36 +0330

    lste Commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const container=document.querySelector(".content");
+for(var i=0; i<=100; i++){
+    const blocks=document.createElement("div");
+    blocks.classList.add('block');
+    container.appendChild(blocks);
+}
+function animeblocks(){
+    anime({
+        targets:".block",
+        translateX:function(){
+            return anime.random(-700,700);
+        },
+        translateY:function(){
+            return anime.random(-500,500);
+        },
+        scale:function(){
+            return anime.random(1,5);
+        },
+        easing:"linear",
+        duration:3000,
+        delay:anime.stagger(10),
+        complete:animeblocks,
+    })
+}
+animeblocks();
